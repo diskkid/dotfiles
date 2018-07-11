2cfc31949244f04dce74fc78fa644f9486937113
Daisuke Koide <disk2id@gmail.com>
2018-07-12 01:40:11 +0900

    Use Ricty Diminished

--- .hyper.js
+++ .hyper.js
@@ -9,10 +9,10 @@ module.exports = {
     updateChannel: 'stable',
 
     // default font size in pixels for all tabs
-    fontSize: 14,
+    fontSize: 16,
 
     // font family with optional fallbacks
-    fontFamily: '"SF Mono Medium", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
+    fontFamily: '"Ricty Diminished for Powerline", Menlo, "DejaVu Sans Mono", Consolas, "Lucida Console", monospace',
 
     // default font weight: 'normal' or 'bold'
     fontWeight: 'normal',
