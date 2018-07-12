a3e072d62187a384cdf925c22f684ca0a7584378
Daisuke Koide <disk2id@gmail.com>
2018-07-12 18:43:34 +0900

    Add plugin installation to install.sh

--- install.sh
+++ install.sh
@@ -2,12 +2,18 @@
 
 ROOT="$(cd $(dirname $0); pwd)"
 
-if [ ! -e ${HOME}/.local/share/zplug ]; then
-  git clone https://github.com/zplug/zplug ${HOME}/.local/share/zplug
+zplug_dir=${HOME}/.local/share/zplug
+if [ ! -e ${zplug_dir} ]; then
+  git clone https://github.com/zplug/zplug ${zplug_dir}
+fi
+
+dein_dir=${HOME}/.local/share/dein/repos/github.com/Shougo/dein.vim
+if [ ! -e ${dein_dir} ]; then
+  git clone https://github.com/Shougo/dein.vim.git ${dein_dir}
 fi
 
 mkdir -p ${HOME}/.config
-ln -s ${ROOT}/.zshrc ${HOME}/
+ln -s ${ROOT}/.config/zsh ${HOME}/.config/
 ln -s ${ROOT}/.hyper.js ${HOME}/
 ln -s ${ROOT}/.config/nvim ${HOME}/.config/
 ln -s ${ROOT}/.config/git ${HOME}/.config/
