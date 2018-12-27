6419b33820441161facce0ff5891c42be1893ab1
Daisuke Koide <disk2id@gmail.com>
2018-12-27 16:20:14 +0000

    Refactor install.sh

--- install.sh
+++ install.sh
@@ -2,18 +2,34 @@
 
 ROOT="$(cd $(dirname $0); pwd)"
 
-zplug_dir=${HOME}/.local/share/zplug
-if [ ! -e ${zplug_dir} ]; then
-  git clone https://github.com/zplug/zplug ${zplug_dir}
-fi
+install_zplug () {
+  local zplug_dir=${HOME}/.local/share/zplug
+  if [ ! -e ${zplug_dir} ]; then
+    git clone https://github.com/zplug/zplug ${zplug_dir}
+  fi
+}
 
-dein_dir=${HOME}/.local/share/dein/repos/github.com/Shougo/dein.vim
-if [ ! -e ${dein_dir} ]; then
-  git clone https://github.com/Shougo/dein.vim.git ${dein_dir}
-fi
+install_dein () {
+  local dein_dir=${HOME}/.local/share/dein/repos/github.com/Shougo/dein.vim
+  if [ ! -e ${dein_dir} ]; then
+    git clone https://github.com/Shougo/dein.vim.git ${dein_dir}
+  fi
+}
 
-mkdir -p ${HOME}/.config
-ln -s ${ROOT}/.zshenv ${HOME}/
-ln -s ${ROOT}/.config/zsh ${HOME}/.config/
-ln -s ${ROOT}/.config/nvim ${HOME}/.config/
-ln -s ${ROOT}/.config/git ${HOME}/.config/
+ln_if_not_exist () {
+  if [ ! -e "$2" ]; then
+    ln -s "$1" "$2"
+  fi
+}
+
+deploy_config () {
+  mkdir -p ${HOME}/.config
+  ln_if_not_exist ${ROOT}/.zshenv ${HOME}/
+  ln_if_not_exist ${ROOT}/.config/zsh ${HOME}/.config/
+  ln_if_not_exist ${ROOT}/.config/nvim ${HOME}/.config/
+  ln_if_not_exist ${ROOT}/.config/git ${HOME}/.config/
+}
+
+install_zplug
+install_dein
+deploy_config
