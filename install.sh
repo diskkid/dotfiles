c909a03ceb20c776394957a6db7dc66de07f03a6
Daisuke Koide <disk2id@gmail.com>
2019-06-08 19:48:26 +0900

    Add mkdir for vscode

--- install.sh
+++ install.sh
@@ -70,6 +70,7 @@ deploy_config () {
   ln_if_not_exist ${ROOT}/.config/zsh ${HOME}/.config/zsh
   ln_if_not_exist ${ROOT}/.config/nvim ${HOME}/.config/nvim
   ln_if_not_exist ${ROOT}/.config/git ${HOME}/.config/git
+  mkdir -p ${HOME}/.config/Code/User
   ln_if_not_exist ${ROOT}/.config/Code/User/settings.json ${HOME}/.config/Code/User/settings.json
 }
 
