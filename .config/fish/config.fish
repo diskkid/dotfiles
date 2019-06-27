bb326c5957ea8df17cb1ace349dc3b5f7956d025
Daisuke Koide <disk2id@gmail.com>
2019-06-28 00:51:34 +0900

    Use -p option

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,32 +9,32 @@ set -x CLICOLOR 1
 #==============================================================================
 # PATH
 #==============================================================================
-set PATH $HOME/.bin $HOME/.local/bin $PATH
-set PATH /usr/local/sbin $PATH
+set -p PATH $HOME/.bin $HOME/.local/bin
+set -p PATH /usr/local/sbin
 # Use coreutils
 # equivalent to export PATH="$(brew --prefix coreutils)/libexec/gnubin:${PATH}"
 if test -e /usr/local/opt/coreutils
-  set PATH /usr/local/opt/coreutils/libexec/gnubin $PATH
-  set MANPATH /usr/local/opt/coreutils/libexec/gnuman $MANPATH
+  set -p PATH /usr/local/opt/coreutils/libexec/gnubin
+  set -p MANPATH /usr/local/opt/coreutils/libexec/gnuman
 end
 
 #==============================================================================
 # Go
 #==============================================================================
 if test -e /usr/local/go
-  set PATH /usr/local/go/bin $PATH
+  set -p PATH /usr/local/go/bin
 end
 
 if which go > /dev/null 2>&1
   set -x GOPATH $HOME/Code/go
-  set PATH $PATH (go env GOPATH)/bin
+  set -p PATH (go env GOPATH)/bin
 end
 
 #==============================================================================
 # Rust
 #==============================================================================
+set -p PATH $HOME/.cargo/bin
 if which rustc > /dev/null 2>&1
-  set PATH $HOME/.cargo/bin $PATH
   set -x RUST_SRC_PATH (rustc --print sysroot)/lib/rustlib/src/rust/src
 end
 
@@ -43,7 +43,7 @@ end
 #==============================================================================
 set -x RBENV_ROOT $XDG_DATA_HOME/rbenv
 if test -e $RBENV_ROOT
-  set PATH $RBENV_ROOT/bin:$PATH
+  set -p PATH $RBENV_ROOT/bin
   status --is-interactive; and source (rbenv init -|psub)
 end
 
@@ -52,7 +52,7 @@ end
 #==============================================================================
 set -x NODENV_ROOT $XDG_DATA_HOME/nodenv
 if test -e $NODENV_ROOT
-  set PATH $NODENV_ROOT/bin:$PATH
+  set -p PATH $NODENV_ROOT/bin
   status --is-interactive; and source (nodenv init -|psub)
 end
 
@@ -72,3 +72,5 @@ end
 if ls --color=auto > /dev/null 2>&1
   alias ls='ls --color=auto'
 end
+
+alias less='less -R'
