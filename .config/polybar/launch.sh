9edcbce69ab627196f9e1abde93d85c4703a66ff
Daisuke Koide <disk2id@gmail.com>
2019-06-30 23:19:32 +0900

    Set up polybar

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,4 +3,10 @@
 killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-polybar example &
+if type xrandr; then
+  for m in $(polybar --list-monitors | cut -d":" -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
