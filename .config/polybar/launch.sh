f588f6c5c45f178a71c340ae688512a408cdaffd
Daisuke Koide <disk2id@gmail.com>
2021-03-13 20:34:43 +0900

    Remove if

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,10 +3,6 @@
 killall -q polybar
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-if type xrandr; then
-  for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar --reload example &
-  done
-else
-  polybar --reload example &
-fi
+for m in $(polybar --list-monitors | cut -d":" -f1); do
+  MONITOR=$m polybar --reload example &
+done
